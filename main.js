07c04b538a268b8b9498fd3020d8062365ce241f
Lord-Ducky <92235419+Lord-Ducky@users.noreply.github.com>
2022-03-14 18:01:41 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var first_name = "Aryan";
+var last_name = "Walia";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 5;
+var b = 6;
+
+var add = a + b;
+var substract = a - b;
+var divide = a/b;
+var multiply = a * b;
+
+console.log(multiply);
+console.log(add);
+console.log(substract);
+console.log(divide);
+
+
+
+
+
